babc911285b01efb8d4203c522036466a5abd006
Thatchapon Unprasert <poomst@hotmail.com>
2016-07-02 21:41:33 +0700

    We should close the app only from ScannerDialog.

--- src/ScannerListDialog.java
+++ src/ScannerListDialog.java
@@ -1,4 +1,6 @@
 import java.awt.GridLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
@@ -9,17 +11,21 @@ import javax.swing.JLabel;
 public class ScannerListDialog extends JFrame {
 
 	private static final long serialVersionUID = 212016L;
-	
+
 	private static final Integer maxListCount = 25;
 
 	private List<Integer> IDs;
 	private Map<Integer, Student> students;
 
 	public ScannerListDialog(Map<Integer, Student> students) {
-		this.setTitle("Scanned codes" + (Main.test ? " (Test Mode)" : ""));
-		this.setLayout(new GridLayout(1 + maxListCount, 1));
-		this.setSize(400, 600);
-		//this.setLocationRelativeTo(null);
+		this.setTitle("Scanned Codes" + (Main.test ? " (Test Mode)" : ""));
+		this.setLayout(new GridLayout(maxListCount, 1));
+		this.setLocation(1200, 320);
+		this.setSize(450, 600);
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {}
+		});
 		this.IDs = new Vector<Integer>();
 		this.students = students;
 	}
@@ -27,14 +33,14 @@ public class ScannerListDialog extends JFrame {
 	public List<Integer> getIDs() {
 		return IDs;
 	}
-	
+
 	public void addID(Integer ID) {
 		Student student = students.get(ID);
 		String slabel = " " + ID;
 		if (student == null)
 			slabel += ": Unknown";
 		else
-			slabel += ": " + student.getName() + " (" + student.getNickname() + ")";
+			slabel += String.format(": %s (%s)", student.getName(), student.getNickname());
 		JLabel label = new JLabel(slabel, JLabel.LEFT);
 		IDs.add(ID);
 		this.getContentPane().add(label);

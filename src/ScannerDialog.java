80a178a479d26ca4510aea3e31809d1c3a4b46ff
Thatchapon Unprasert <poomst@hotmail.com>
2016-07-03 09:22:31 +0700

    Actions for regular and absence approach.

--- src/ScannerDialog.java
+++ src/ScannerDialog.java
@@ -4,12 +4,17 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.util.Map;
 import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.event.DocumentEvent;
@@ -23,18 +28,60 @@ public class ScannerDialog extends JFrame {
 
 	private Vector<Integer> IDs;
 	private static final String pMUICTID = "\\d\\d88\\d\\d\\d";
+	private static final Pattern pDelAtIndex = Pattern.compile("del(\\d+)");
 
 	private ScannerListDialog list;
 	private Map<Integer, Student> students;
 
 	private JLabel statusLabel;
 	private JTextField field;
-	private JButton confirmBtn;
+	private JButton confirmRegularBtn;
+	private JButton appendRegularBtn;
+	private JButton confirmNotHereBtn;
+	private JButton appendNotHereBtn;
+	
+	private void destroyEverything() {
+		this.setVisible(false);
+		this.dispose();
+		list.setVisible(false);
+		list.dispose();
+	}
+	
+	private void actionPerformWriteForType(String title, String confirmString, String fileExistedString, ScannerSaver.Type type) {
+		try {
+			int result = JOptionPane.showConfirmDialog(null, confirmString, title, JOptionPane.YES_NO_OPTION);
+			if (result == JOptionPane.YES_OPTION) {
+				ScannerSaver.doneAddingCode(IDs, false, type);
+			}
+		} catch (FileAlreadyExistsException ex) {
+			int result = JOptionPane.showConfirmDialog(null, fileExistedString, title, JOptionPane.YES_NO_OPTION);
+			if (result == JOptionPane.YES_OPTION) {
+				try {
+					ScannerSaver.doneAddingCode(IDs, false, type, true);
+				} catch (IOException ex2) {
+					ex2.printStackTrace();
+				}
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	private void actionPerformAppendForType(String title, String confirmString, ScannerSaver.Type type) {
+		try {
+			int result = JOptionPane.showConfirmDialog(null, confirmString, title, JOptionPane.YES_NO_OPTION);
+			if (result == JOptionPane.YES_OPTION) {
+				ScannerSaver.doneAddingCode(IDs, true, type);
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
 
 	public ScannerDialog(Map<Integer, Student> students) {
 		this.setTitle("Scanner" + (Main.test ? " (Test Mode)" : ""));
-		this.setLayout(new GridLayout(4, 1));
-		this.setSize(400, 160);
+		this.setLayout(new GridLayout(7, 1));
+		this.setSize(400, 200);
 		field = new JTextField(1);
 		field.setHorizontalAlignment(JTextField.CENTER);
 		AbstractDocument document = (AbstractDocument) (field.getDocument());
@@ -59,10 +106,14 @@ public class ScannerDialog extends JFrame {
 			}
 
 			public void validate() {
-				confirmBtn.setEnabled(!IDs.isEmpty());
+				confirmRegularBtn.setEnabled(!IDs.isEmpty());
+				appendRegularBtn.setEnabled(!IDs.isEmpty());
+				confirmNotHereBtn.setEnabled(!IDs.isEmpty());
+				appendNotHereBtn.setEnabled(!IDs.isEmpty());
 				String text = field.getText();
 				boolean shouldCleanup = false;
 				boolean removeLabel = false;
+				Matcher m;
 				if (text.equals("flush")) {
 					list.removeAll();
 					IDs.removeAllElements();
@@ -71,6 +122,13 @@ public class ScannerDialog extends JFrame {
 					list.removeLast();
 					IDs.remove(IDs.lastElement());
 					shouldCleanup = removeLabel = true;
+				} else if ((m = (pDelAtIndex.matcher(text))).find()) {
+					int idx = Integer.parseInt(m.group(1));
+					list.removeAtIndex(idx);
+					removeLabel = idx == IDs.size() - 1;
+					if (idx < IDs.size())
+						IDs.remove(idx);
+					shouldCleanup = true;
 				} else {
 					String ID = null;
 					if (!Main.test) {
@@ -119,8 +177,7 @@ public class ScannerDialog extends JFrame {
 		field.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("Enter pressed");
-				setVisible(false);
-				dispose();
+				destroyEverything();
 			}
 		});
 
@@ -134,21 +191,45 @@ public class ScannerDialog extends JFrame {
 		statusLabel.setForeground(Color.gray);
 		this.getContentPane().add(statusLabel);
 
-		confirmBtn = new JButton("Confirm data");
-		confirmBtn.setEnabled(false);
-		confirmBtn.setForeground(Color.blue);
-		confirmBtn.addActionListener(new ActionListener() {
+		confirmRegularBtn = new JButton("Confirm regular data");
+		confirmRegularBtn.setEnabled(false);
+		confirmRegularBtn.setForeground(Color.blue);
+		confirmRegularBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				try {
-					ScannerSaver.doneAddingCode(IDs, false);
-					setVisible(false);
-					dispose();
-				} catch (Exception ex) {
-					ex.printStackTrace();
-				}
+				actionPerformWriteForType("Scanner", "Confirm writing data?", "File already existed, continue?", ScannerSaver.Type.REGULAR);
 			}
 		});
-		this.getContentPane().add(confirmBtn);
+		this.getContentPane().add(confirmRegularBtn);
+		
+		appendRegularBtn = new JButton("Append regular data");
+		appendRegularBtn.setEnabled(false);
+		appendRegularBtn.setForeground(Color.red);
+		appendRegularBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				actionPerformAppendForType("Scanner", "Confirm append data?", ScannerSaver.Type.REGULAR);
+			}
+		});
+		this.getContentPane().add(appendRegularBtn);
+		
+		confirmNotHereBtn = new JButton("Confirm absence data");
+		confirmNotHereBtn.setEnabled(false);
+		confirmNotHereBtn.setForeground(Color.blue);
+		confirmNotHereBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				actionPerformWriteForType("Scanner", "Confirm writing absence data?", "File already existed, continue?", ScannerSaver.Type.NOTHERE);
+			}
+		});
+		this.getContentPane().add(confirmNotHereBtn);
+		
+		appendNotHereBtn = new JButton("Append absence data");
+		appendNotHereBtn.setEnabled(false);
+		appendNotHereBtn.setForeground(Color.red);
+		appendNotHereBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				actionPerformAppendForType("Scanner", "Confirm append absence data?", ScannerSaver.Type.NOTHERE);
+			}
+		});
+		this.getContentPane().add(appendNotHereBtn);
 
 		this.students = students;
 	}
--- src/ScannerSaver.java
+++ src/ScannerSaver.java
@@ -1,15 +1,35 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.util.Vector;
 
 import org.apache.commons.io.FileUtils;
 
 public class ScannerSaver {
-
-	public static void doneAddingCode(Vector<Integer> IDs, boolean append) throws Exception {
-		String filePath = "Attendance/" + DateUtils.getCurrentFormattedDate() + "/present.csv";
+	
+	public enum Type { REGULAR, NOTHERE }
+	
+	private static String filename(Type type) throws FileNotFoundException {
+		switch (type) {
+		case REGULAR:
+			return "present.csv";
+		case NOTHERE:
+			return "absent.csv";
+		}
+		throw new FileNotFoundException("");
+	}
+	
+	public static void doneAddingCode(Vector<Integer> IDs, boolean append, Type type, boolean force) throws IOException {
+		String filePath = "Attendance/" + DateUtils.getCurrentFormattedDate() + "/" + filename(type);
 		File file = new File(filePath);
-		if (file.exists() && !append)
-			throw new Exception("File already existed: " + filePath);
+		if (file.exists() && !append && !force)
+			throw new FileAlreadyExistsException("File already existed: " + filePath);
 		FileUtils.writeLines(file, IDs, append);
+		System.out.println(String.format("%s data to %s", (append ? "Append" : "Write"), filePath));
+	}
+	
+	public static void doneAddingCode(Vector<Integer> IDs, boolean append, Type type) throws IOException {
+		doneAddingCode(IDs, append, type, false);
 	}
 }

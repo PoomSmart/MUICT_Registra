696f689c2cd80e8afd0122c1120e10f220e90a41
Thatchapon Unprasert <poomst@hotmail.com>
2016-07-04 11:03:49 +0700

    Move pattern here.

--- src/CommonUtils.java
+++ src/CommonUtils.java
@@ -2,6 +2,8 @@ import java.io.File;
 
 public class CommonUtils {
 	
+	public static final String pMUICTID = "\\d\\d88\\d\\d\\d";
+	
 	public static boolean fileExistsAtPath(String filePath) {
 		return new File(filePath).exists();
 	}

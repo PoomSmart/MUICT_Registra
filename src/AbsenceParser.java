6dd7e8a8b2ace1cd4619f261a3b5391414b8a14a
Thatchapon Unprasert <poomst@hotmail.com>
2016-07-04 21:46:17 +0700

    Bug fix.

--- src/AbsenceParser.java
+++ src/AbsenceParser.java
@@ -12,18 +12,19 @@ public class AbsenceParser {
 	// Pattern: ID,type,reason
 	private static final Pattern pDB = Pattern.compile("(\\d+),(.*),(.*)");
 	
-	private Map<Integer, Student> absentStudents = new TreeMap<Integer, Student>();
+	private Map<Integer, Student> absentStudents;
 
 	public AbsenceParser(String dbPath, Map<Integer, Student> students) {
 		try {
+			absentStudents = new TreeMap<Integer, Student>();
 			BufferedReader reader = new BufferedReader(new FileReader(new File(dbPath)));
 			String line;
 			Matcher m;
 			while ((line = reader.readLine()) != null) {
 				if ((m = pDB.matcher(line)).find()) {
 					Integer ID = Integer.parseInt(m.group(1));
-					Status.Type type = Status.getType(m.group(1));
-					String reason = m.group(2);
+					Status.Type type = Status.getType(m.group(2));
+					String reason = m.group(3);
 					if (!students.containsKey(ID)) {
 						System.out.println("ID not found: " + ID);
 						System.out.println("I don't think we would face this problem");
@@ -37,6 +38,10 @@ public class AbsenceParser {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	public Map<Integer, Student> getAbsentStudents() {
+		return absentStudents;
 	}
 
 }

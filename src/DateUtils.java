e5598499564a51d6b6444134154de21921ccdd44
Thatchapon Unprasert <poomst@hotmail.com>
2016-07-04 18:43:38 +0700

    A user-friendly date format.

--- src/DateUtils.java
+++ src/DateUtils.java
@@ -4,10 +4,11 @@ import java.util.Date;
 
 public class DateUtils {
 	
-	private static final SimpleDateFormat fmt = new SimpleDateFormat("yyyyMMdd");
+	private static final SimpleDateFormat s_fmt = new SimpleDateFormat("yyyyMMdd");
+	private static final SimpleDateFormat n_fmt = new SimpleDateFormat("dd/MM/yyyy");
 	
 	public static String formattedDate(Date date) {
-		return fmt.format(date);
+		return s_fmt.format(date);
 	}
 	
 	public static Date getCurrentDate() {
@@ -18,5 +19,13 @@ public class DateUtils {
 	
 	public static String getCurrentFormattedDate() {
 		return formattedDate(getCurrentDate());
+	}
+	
+	public static String normalFormattedDate(Date date) {
+		return n_fmt.format(date);
+	}
+	
+	public static String getCurrentNormalFormattedDate() {
+		return normalFormattedDate(getCurrentDate());
 	}
 }

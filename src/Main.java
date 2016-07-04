4e587e01d81657d901eee5a57ec78988c63314b1
Thatchapon Unprasert <poomst@hotmail.com>
2016-07-04 18:44:40 +0700

    Simplified.

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,48 @@
 import java.io.IOException;
 import java.util.Map;
 
+import javax.swing.JFrame;
+
 public class Main {
 	
 	public static final boolean test = true;
+	
+	private static void runFrame(JFrame frame) throws InterruptedException {
+		Thread t = new Thread() {
+			public void run() {
+				synchronized (frame) {
+					while (frame.isVisible()) {
+						try {
+							frame.wait();
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		};
+		t.join();
+	}
 
 	public static void main(String[] args) throws InterruptedException, IOException {
-		
 		Map<Integer, Student> students = new StudentDatabase("batch-14.csv").getStudents();
 		
 		ScannerDialog scannerDialog = new ScannerDialog(students);
 		scannerDialog.setVisible(true);
-		Thread t1 = new Thread() {
-			public void run() {
-				synchronized (scannerDialog) {
-					while (scannerDialog.isVisible()) {
-						try {
-							scannerDialog.wait();
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
-				}
-			}
-		};
-		t1.join();
+		runFrame(scannerDialog);
+		
 		ScannerListDialog scannerListDialog = new ScannerListDialog(students);
 		scannerListDialog.setVisible(true);
 		scannerDialog.setList(scannerListDialog);
-		Thread t2 = new Thread() {
-			public void run() {
-				synchronized (scannerListDialog) {
-					while (scannerListDialog.isVisible()) {
-						try {
-							scannerListDialog.wait();
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
-				}
-			}
-		};
-		t2.join();
+		runFrame(scannerListDialog);
 		
 		LeaveDialog leaveDialog = new LeaveDialog(students);
 		leaveDialog.setVisible(true);
-		Thread t3 = new Thread() {
-			public void run() {
-				synchronized (leaveDialog) {
-					while (leaveDialog.isVisible()) {
-						try {
-							leaveDialog.wait();
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
-					}
-				}
-			}
-		};
-		t3.join();
+		runFrame(leaveDialog);
+		
+		ControlCenterDialog ccDialog = new ControlCenterDialog(students);
+		ccDialog.setVisible(true);
+		runFrame(ccDialog);
 		
 		scannerDialog.toFront();
 		scannerDialog.getField().requestFocus();

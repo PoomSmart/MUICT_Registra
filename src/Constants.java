72a1b2950a78c32cca6f5e088cb6afc2d701c4f1
Thatchapon Unprasert <poomst@hotmail.com>
2016-07-04 07:53:10 +0700

    Constants for this project.

---
+++ src/Constants.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Constants {
+	private static final String[] _flushStrings = { "flush", "remall", "null", "clean" };
+	public static final List<String> flushStrings = Arrays.asList(_flushStrings);
+	private static final String[] _delLastStrings = { "dellast", "pop" };
+	public static final List<String> delLastStrings = Arrays.asList(_delLastStrings);
+	
+	public static final String CONFIRM_WRITE_REGULAR = "Confirm writing regular data?";
+	public static final String CONFIRM_WRITE_ABSENCE = "Confirm writing absence data?";
+	public static final String CONFIRM_APPEND_REGULAR = "Confirm append regular data?";
+	public static final String CONFIRM_APPEND_ABSENCE = "Confirm append absence data?";
+	
+	public static final String CONFIRM_OVERWRITE = "File already existed, continue?";
+	
+	public static final String SCANNER_DIALOG_TITLE = "Scanner";
+}

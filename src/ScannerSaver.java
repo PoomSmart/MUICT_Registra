1f830fa766026b458c5fde6dac26559319ef1eef
Thatchapon Unprasert <poomst@hotmail.com>
2016-07-04 08:14:00 +0700

    Factoring.

--- src/ScannerSaver.java
+++ src/ScannerSaver.java
@@ -20,13 +20,19 @@ public class ScannerSaver {
 		throw new FileNotFoundException("");
 	}
 	
+	public static String attendancePath(Type type) throws FileNotFoundException {
+		return "Attendance/" + DateUtils.getCurrentFormattedDate() + "/" + filename(type);
+	}
+	
 	public static void doneAddingCode(Vector<Integer> IDs, boolean append, Type type, boolean force) throws IOException {
-		String filePath = "Attendance/" + DateUtils.getCurrentFormattedDate() + "/" + filename(type);
+		String filePath = attendancePath(type);
 		File file = new File(filePath);
 		if (file.exists() && !append && !force)
 			throw new FileAlreadyExistsException("File already existed: " + filePath);
+		/*if (!file.exists() && append)
+			System.out.println("Note: appending file where it does not exist");*/
 		FileUtils.writeLines(file, IDs, append);
-		System.out.println(String.format("%s data to %s", (append ? "Append" : "Write"), filePath));
+		System.out.println(String.format("(%s mode) %s data to %s", type, (append ? "Append" : "Write"), filePath));
 	}
 	
 	public static void doneAddingCode(Vector<Integer> IDs, boolean append, Type type) throws IOException {

48cdbfccab25f32de7479203cc76bdf189f3bb1f
Thatchapon Unprasert <poomst@hotmail.com>
2016-07-05 21:38:10 +0700

    Fix random algorithm.

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,6 +3,7 @@ package Main;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Random;
+import java.util.Vector;
 
 import javax.swing.JFrame;
 
@@ -13,6 +14,7 @@ import Dialogs.ScannerDialog;
 import Dialogs.ScannerListDialog;
 import Objects.Position;
 import Objects.Student;
+import Utilities.CommonUtils;
 import Visualizers.SeatVisualizer;
 
 public class Main {
@@ -20,6 +22,7 @@ public class Main {
 	public static final boolean test = true;
 
 	private static void runFrame(JFrame frame) throws InterruptedException {
+		CommonUtils.setDontClose(frame);
 		Thread t = new Thread() {
 			public void run() {
 				synchronized (frame) {
@@ -38,18 +41,21 @@ public class Main {
 
 	// FIXME: Will be eventually removed
 	private static void randomPosition(Map<Integer, Student> students) {
-		Random r = new Random();
 		Integer width = SeatVisualizer.bounds.width;
 		Integer height = SeatVisualizer.bounds.height;
+		if (width * height < students.size()) {
+			System.out.println("Couldn't random (W x H < Map Size)");
+			return;
+		}
+		Random r = new Random();
+		Vector<Position<Integer, Integer>> positions = new Vector<Position<Integer, Integer>>();
 		for (Student student : students.values()) {
 			if (student.getPosition().equals(Position.nullPosition)) {
 				Position<Integer, Integer> position = new Position<>(r.nextInt(width), r.nextInt(height));
-				for (Student student2 : students.values()) {
-					if (!student.equals(student2)) {
-						if (!position.equals(student2.getPosition()))
-							student.setPosition(position);
-					}
-				}
+				if (positions.contains(position))
+					continue;
+				positions.add(position);
+				student.setPosition(position);
 			}
 		}
 	}
@@ -74,6 +80,10 @@ public class Main {
 		ControlCenterDialog ccDialog = new ControlCenterDialog(students);
 		ccDialog.setVisible(true);
 		runFrame(ccDialog);
+		
+		SeatVisualizer vis = new SeatVisualizer(students);
+		vis.setVisible(true);
+		runFrame(vis);
 
 		scannerDialog.toFront();
 		scannerDialog.getField().requestFocus();

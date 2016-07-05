ddfbf886a6a1de34fcc8c5afb26b4cf2c373954f
Thatchapon Unprasert <poomst@hotmail.com>
2016-07-05 16:45:35 +0700

    Reloadable tiles.

--- src/Visualizers/SeatVisualizer.java
+++ src/Visualizers/SeatVisualizer.java
@@ -4,7 +4,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
-import java.awt.Rectangle;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.Map;
 
 import javax.swing.JFrame;
@@ -14,8 +15,6 @@ import Objects.Position;
 import Objects.Student;
 import Tables.StudentTable;
 import Utilities.CommonUtils;
-import Utilities.CommonUtils.FileType;
-import Utilities.DateUtils;
 
 class SeatPanel extends JPanel {
 
@@ -27,6 +26,10 @@ class SeatPanel extends JPanel {
 		this.students = students;
 	}
 
+	public void setStudents(Map<Integer, Student> students) {
+		this.students = students;
+	}
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
@@ -34,11 +37,15 @@ class SeatPanel extends JPanel {
 		int tileHeight = SeatVisualizer.tileSize.height;
 		for (int x = 0; x < SeatVisualizer.bounds.width; x++) {
 			for (int y = 0; y < SeatVisualizer.bounds.height; y++) {
+				g.setColor(Color.white);
 				boolean found = false;
 				for (Student student : students.values()) {
 					Position<Integer, Integer> position = student.getPosition();
 					if (position.equals(new Position<Integer, Integer>(x, y))) {
-						g.setColor(Color.blue);
+						if (student.isNormal())
+							g.setColor(Color.blue);
+						else if (student.isLeft())
+							g.setColor(Color.yellow);
 						found = true;
 						break;
 					}
@@ -53,36 +60,67 @@ class SeatPanel extends JPanel {
 			g.drawLine(x * tileWidth, 0, x * tileWidth, SeatVisualizer.absoluteSize.height);
 		}
 		g.setColor(Color.gray);
-		g.drawLine(SeatVisualizer.absoluteSize.width, 0, SeatVisualizer.absoluteSize.width, SeatVisualizer.absoluteSize.height);
-		g.drawLine(0, SeatVisualizer.absoluteSize.height, SeatVisualizer.absoluteSize.width, SeatVisualizer.absoluteSize.height);
+		g.drawLine(SeatVisualizer.absoluteSize.width, 0, SeatVisualizer.absoluteSize.width,
+				SeatVisualizer.absoluteSize.height);
+		g.drawLine(0, SeatVisualizer.absoluteSize.height, SeatVisualizer.absoluteSize.width,
+				SeatVisualizer.absoluteSize.height);
 	}
 
 }
 
 public class SeatVisualizer extends JFrame {
+
+	// FIXME: Fix bug with leave-with-reason students calculation
+	// TODO: Fix up coloring
+
 	private static final long serialVersionUID = 1L;
 
-	private Map<Integer, Student> students;
+	public static SeatVisualizer activeVisualizer = null;
+
 	public static final Dimension bounds = new Dimension(12, 20);
 	public static final Dimension tileSize = new Dimension(40, 25);
 	public static final Dimension absoluteSize = new Dimension(bounds.width * tileSize.width,
 			bounds.height * tileSize.height);
 
+	private SeatPanel panel;
+	private Map<Integer, Student> currentStudents;
+	private Map<Integer, Student> students;
+
 	public SeatVisualizer(Map<Integer, Student> students) {
 		this.setTitle(CommonUtils.realTitle("Seat Visualizer"));
-		this.setSize(absoluteSize.width, absoluteSize.height + (int)(tileSize.height * 1.5));
+		this.setSize(absoluteSize.width, absoluteSize.height + (int) (tileSize.height * 1.5));
 		getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 0, 1));
-		CommonUtils.setCenter(this);
-		StudentTable table = new StudentTable(students, 0);
-		Map<Integer, Student> currentStudents = table.studentMapForDate(DateUtils.getCurrentDate(), FileType.REGULAR);
+		CommonUtils.setRelativeCenter(this, -480, 0);
+		this.students = students;
+		reloadStudents();
 		if (currentStudents != null) {
-			SeatPanel panel = new SeatPanel(students = currentStudents);
+			panel = new SeatPanel(currentStudents);
 			panel.setPreferredSize(this.getSize());
 			this.setPreferredSize(this.getSize());
 			this.setMinimumSize(this.getSize());
 			this.setMaximumSize(this.getSize());
 			this.add(panel);
 			this.setResizable(false);
+			activeVisualizer = this;
+		}
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				activeVisualizer = null;
+			}
+		});
+	}
+
+	private void reloadStudents() {
+		StudentTable table = new StudentTable(students, 0);
+		currentStudents = table.getInternalStudents();
+	}
+
+	public static void updateIfPossible() {
+		System.out.println("Updating SeatVisualizer");
+		if (activeVisualizer != null) {
+			activeVisualizer.reloadStudents();
+			activeVisualizer.panel.setStudents(activeVisualizer.currentStudents);
+			activeVisualizer.repaint();
 		}
 	}
 

dd090b0ea56b88857ce8928b687d9c7cef9a14cc
Thatchapon Unprasert <poomst@hotmail.com>
2016-07-05 16:44:07 +0700

    Interactive description label.

--- src/Dialogs/ScannerDialog.java
+++ src/Dialogs/ScannerDialog.java
@@ -1,15 +1,19 @@
 package Dialogs;
+
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -28,7 +32,10 @@ import javax.swing.text.DocumentFilter;
 import Main.Main;
 import Objects.Constants;
 import Objects.Student;
+import Tables.StudentTable;
 import Utilities.CommonUtils;
+import Utilities.CommonUtils.FileType;
+import Utilities.DateUtils;
 import Workers.ScannerSaver;
 
 public class ScannerDialog extends JFrame {
@@ -40,10 +47,14 @@ public class ScannerDialog extends JFrame {
 
 	private ScannerListDialog list;
 
+	private JLabel desLabel;
 	private JLabel statusLabel;
 	private JTextField field;
 	private JButton confirmRegularBtn;
 	private JButton appendRegularBtn;
+
+	private Map<Integer, Student> students;
+	private Map<Integer, Student> currentPresentStudents;
 
 	private void destroyEverything() {
 		this.setVisible(false);
@@ -68,20 +79,36 @@ public class ScannerDialog extends JFrame {
 		});
 	}
 
+	private void resolveIDDuplicates() {
+		for (Integer ID : currentPresentStudents.keySet())
+			IDs.remove(ID);
+	}
+
+	private void reloadCurrentPresentStudents() {
+		StudentTable table = new StudentTable(students, 0);
+		currentPresentStudents = table.presentStudentMapForDate(DateUtils.getCurrentDate());
+		if (currentPresentStudents == null)
+			currentPresentStudents = new TreeMap<Integer, Student>();
+	}
+
 	private void actionPerformWriteForType(String title, String confirmString, String fileExistedString,
 			CommonUtils.FileType type) {
 		try {
 			int result = JOptionPane.showConfirmDialog(null, confirmString, title, JOptionPane.YES_NO_OPTION);
 			if (result == JOptionPane.YES_OPTION) {
+				resolveIDDuplicates();
 				ScannerSaver.doneAddingCodes(IDs, false, type);
 				cleanup();
+				reloadCurrentPresentStudents();
 			}
 		} catch (FileAlreadyExistsException ex) {
 			int result = JOptionPane.showConfirmDialog(null, fileExistedString, title, JOptionPane.YES_NO_OPTION);
 			if (result == JOptionPane.YES_OPTION) {
 				try {
+					resolveIDDuplicates();
 					ScannerSaver.doneAddingCodes(IDs, false, type, true);
 					cleanup();
+					reloadCurrentPresentStudents();
 				} catch (IOException ex2) {
 					ex2.printStackTrace();
 				}
@@ -95,30 +122,35 @@ public class ScannerDialog extends JFrame {
 		try {
 			int result = JOptionPane.showConfirmDialog(null, confirmString, title, JOptionPane.YES_NO_OPTION);
 			if (result == JOptionPane.YES_OPTION) {
+				resolveIDDuplicates();
 				ScannerSaver.doneAddingCodes(IDs, true, type);
+				cleanup();
+				reloadCurrentPresentStudents();
 			}
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
 	}
-	
+
 	private void updateButtonsState() {
 		boolean isIDNotEmpty = !IDs.isEmpty();
 		confirmRegularBtn.setEnabled(isIDNotEmpty);
-		appendRegularBtn.setEnabled(isIDNotEmpty && CommonUtils.fileExistsAtPath(CommonUtils.filePath(CommonUtils.FileType.REGULAR)));
+		appendRegularBtn
+				.setEnabled(isIDNotEmpty && CommonUtils.fileExistsAtPath(CommonUtils.filePath(FileType.REGULAR)));
 	}
 
 	public ScannerDialog(Map<Integer, Student> students) {
+		this.students = students;
 		this.setTitle(CommonUtils.realTitle(Constants.SCANNER_DIALOG_TITLE));
 		this.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 		this.setSize(400, 150);
-		field = new JTextField(1);
+		field = new JTextField();
 		field.setHorizontalAlignment(JTextField.CENTER);
 		AbstractDocument document = (AbstractDocument) (field.getDocument());
 		document.setDocumentFilter(new DocumentFilter());
 		this.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent windowEvent) {
+			public void windowClosing(WindowEvent e) {
 				System.out.println("Exit");
 				System.exit(0);
 			}
@@ -192,7 +224,7 @@ public class ScannerDialog extends JFrame {
 							} else {
 								IDs.add(ID);
 								list.addID(ID);
-								setStatus("Added " + ID);
+								setStatus("Added: " + ID);
 							}
 						}
 						shouldCleanup = true;
@@ -211,20 +243,29 @@ public class ScannerDialog extends JFrame {
 				destroyEverything();
 			}
 		});
+		field.addFocusListener(new FocusListener() {
+			public void focusGained(FocusEvent e) {
+				desLabel.setText(Constants.SCANNER_DETECTING_MESSAGE);
+			}
+
+			public void focusLost(FocusEvent e) {
+				desLabel.setText(" ");
+			}
+		});
 
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = c.gridy = 0;
 		c.gridwidth = 5;
 		c.ipady = 5;
-		this.getContentPane().add(field, c);
+		getContentPane().add(field, c);
 
-		JLabel des = new JLabel("Detecting scanned code", JLabel.CENTER);
+		desLabel = new JLabel(Constants.SCANNER_DETECTING_MESSAGE, JLabel.CENTER);
 		c.gridx = 0;
 		c.gridy = 1;
 		c.weightx = 1;
 		c.gridwidth = 5;
 		c.ipady = 10;
-		this.getContentPane().add(des, c);
+		getContentPane().add(desLabel, c);
 
 		statusLabel = new JLabel(" ", JLabel.CENTER);
 		statusLabel.setForeground(Color.gray);
@@ -232,7 +273,7 @@ public class ScannerDialog extends JFrame {
 		c.gridy = 2;
 		c.gridwidth = 5;
 		c.ipady = 10;
-		this.getContentPane().add(statusLabel, c);
+		getContentPane().add(statusLabel, c);
 
 		c.gridx = 0;
 		c.gridy = 3;
@@ -240,39 +281,42 @@ public class ScannerDialog extends JFrame {
 		c.ipady = 0;
 		JLabel regularLabel = new JLabel("Present:");
 		regularLabel.setHorizontalAlignment(JLabel.CENTER);
-		this.getContentPane().add(regularLabel, c);
+		getContentPane().add(regularLabel, c);
 
 		confirmRegularBtn = new JButton("Confirm");
 		confirmRegularBtn.setEnabled(false);
 		confirmRegularBtn.setForeground(Color.blue);
 		confirmRegularBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				actionPerformWriteForType(Constants.SCANNER_DIALOG_TITLE, Constants.CONFIRM_WRITE_REGULAR, Constants.CONFIRM_OVERWRITE,
-						CommonUtils.FileType.REGULAR);
+				actionPerformWriteForType(Constants.SCANNER_DIALOG_TITLE, Constants.CONFIRM_WRITE_REGULAR,
+						Constants.CONFIRM_OVERWRITE, CommonUtils.FileType.REGULAR);
 			}
 		});
 		c.gridx = 1;
 		c.gridy = 3;
 		c.gridwidth = 2;
 		c.insets = new Insets(3, 5, 3, 5);
-		this.getContentPane().add(confirmRegularBtn, c);
+		getContentPane().add(confirmRegularBtn, c);
 
 		appendRegularBtn = new JButton("Append");
 		appendRegularBtn.setEnabled(false);
 		appendRegularBtn.setForeground(Color.red);
 		appendRegularBtn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				actionPerformAppendForType(Constants.SCANNER_DIALOG_TITLE, Constants.CONFIRM_APPEND_REGULAR, CommonUtils.FileType.REGULAR);
+				actionPerformAppendForType(Constants.SCANNER_DIALOG_TITLE, Constants.CONFIRM_APPEND_REGULAR,
+						CommonUtils.FileType.REGULAR);
 			}
 		});
 		c.gridx = 3;
 		c.gridy = 3;
 		c.gridwidth = 2;
-		this.getContentPane().add(appendRegularBtn, c);
+		getContentPane().add(appendRegularBtn, c);
 
 		CommonUtils.setRelativeCenter(this, 0, -50);
 		this.setResizable(false);
 		this.IDs = new Vector<Integer>();
+
+		reloadCurrentPresentStudents();
 	}
 
 	public Vector<Integer> getIDs() {

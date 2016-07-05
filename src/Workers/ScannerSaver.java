4524331c1a178ac68004071086c744697f57d605
Thatchapon Unprasert <poomst@hotmail.com>
2016-07-05 16:45:57 +0700

    Reduced.

--- src/Workers/ScannerSaver.java
+++ src/Workers/ScannerSaver.java
@@ -9,18 +9,26 @@ import java.util.Vector;
 import org.apache.commons.io.FileUtils;
 
 import Utilities.CommonUtils;
+import Utilities.CommonUtils.FileType;
+import Visualizers.SeatVisualizer;
 
 public class ScannerSaver {
 	
-	public static void doneAddingCodes(Vector<Integer> IDs, Vector<String> reasons, boolean append, CommonUtils.FileType type, boolean force) throws IOException {
+	// TODO: Resolve duplicate IDs in file
+	
+	public static void doneAddingCodes(Vector<Integer> IDs, Vector<String> reasons, boolean append, FileType type, boolean force) throws IOException {
+		if (type == FileType.LOG)
+			return;
 		String filePath = CommonUtils.filePath(type);
 		File file = new File(filePath);
 		if (file.exists() && !append && !force)
 			throw new FileAlreadyExistsException("File already existed: " + filePath);
 		if (!file.exists() && append)
 			System.out.println("Note: appending file where it does not exist");
-		if (type == CommonUtils.FileType.REGULAR)
-			FileUtils.writeLines(file, IDs, append);
+		if (type == CommonUtils.FileType.REGULAR) {
+			System.out.println(String.format("Write code data to %s (type: %s, append: %s, force: %s)", filePath, type, append, force));
+			FileUtils.writeLines(file, CommonUtils.resolveDuplicates(IDs), append);
+		}
 		else {
 			if (reasons == null) {
 				System.out.println("null reason");
@@ -36,21 +44,22 @@ public class ScannerSaver {
 			writer.close();
 		}
 		System.out.println(String.format("(%s mode) %s data to %s", type, (append ? "Append" : "Write"), filePath));
+		SeatVisualizer.updateIfPossible();
 	}
 	
-	public static void doneAddingCodes(Vector<Integer> IDs, boolean append, CommonUtils.FileType type, boolean force) throws IOException {
+	public static void doneAddingCodes(Vector<Integer> IDs, boolean append, FileType type, boolean force) throws IOException {
 		doneAddingCodes(IDs, null, append, type, force);
 	}
 	
-	public static void doneAddingCodes(Vector<Integer> IDs, Vector<String> reasons, boolean append, CommonUtils.FileType type) throws IOException {
+	public static void doneAddingCodes(Vector<Integer> IDs, Vector<String> reasons, boolean append, FileType type) throws IOException {
 		doneAddingCodes(IDs, reasons, append, type, false);
 	}
 	
-	public static void doneAddingCodes(Vector<Integer> IDs, boolean append, CommonUtils.FileType type) throws IOException {
+	public static void doneAddingCodes(Vector<Integer> IDs, boolean append, FileType type) throws IOException {
 		doneAddingCodes(IDs, append, type, false);
 	}
 	
-	public static void doneAddingCode(Integer ID, String reason, boolean append, CommonUtils.FileType type) throws IOException {
+	public static void doneAddingCode(Integer ID, String reason, boolean append, FileType type) throws IOException {
 		Vector<Integer> IDs = new Vector<Integer>();
 		IDs.addElement(ID);
 		Vector<String> reasons = new Vector<String>();

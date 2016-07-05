7cde15c6fdbf3fa5dcbbe9685f26f64088df58b3
Thatchapon Unprasert <poomst@hotmail.com>
2016-07-05 21:37:39 +0700

    Save dialog only if text has been changed.

--- src/Dialogs/PlainTextDialog.java
+++ src/Dialogs/PlainTextDialog.java
@@ -1,4 +1,5 @@
 package Dialogs;
+
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
@@ -16,51 +17,57 @@ import Utilities.CommonUtils;
 public class PlainTextDialog extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private JTextArea textArea;
 	private String filePath;
-	
-	public PlainTextDialog(String title, int width, int height, int margin, String text, boolean editable, String filePath) {
+	private String initialText;
+
+	public PlainTextDialog(String title, int width, int height, int margin, String text, boolean editable,
+			String filePath) {
 		this.filePath = filePath;
 		this.setTitle(title);
 		this.setSize(width, height);
 		CommonUtils.setCenter(this);
-		
+
 		SpringLayout layout = new SpringLayout();
 		getContentPane().setLayout(layout);
-		
+
 		textArea = new JTextArea();
-		textArea.setText(text);
+		textArea.setText(initialText = text);
 		textArea.setEditable(editable);
-		
+
 		layout.putConstraint(SpringLayout.WEST, textArea, margin, SpringLayout.WEST, getContentPane());
 		layout.putConstraint(SpringLayout.EAST, textArea, -margin, SpringLayout.EAST, getContentPane());
 		layout.putConstraint(SpringLayout.NORTH, textArea, margin, SpringLayout.NORTH, getContentPane());
 		layout.putConstraint(SpringLayout.SOUTH, textArea, -margin, SpringLayout.SOUTH, getContentPane());
-		
+
 		getContentPane().add(textArea);
-		
+
 		this.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent windowEvent) {
 				if (editable && filePath != null) {
-					int result = JOptionPane.showConfirmDialog(null, "Apply changes?", title, JOptionPane.YES_NO_OPTION);
-					if (result == JOptionPane.YES_OPTION) {
-						try {
-							FileUtils.write(new File(filePath), textArea.getText());
-						} catch (IOException e) {
-							JOptionPane.showMessageDialog(null, "Error saving file to Path: " + filePath);
-							e.printStackTrace();
+					String newText = textArea.getText();
+					if (!initialText.equals(newText)) {
+						int result = JOptionPane.showConfirmDialog(null, "Apply changes?", title,
+								JOptionPane.YES_NO_OPTION);
+						if (result == JOptionPane.YES_OPTION) {
+							try {
+								FileUtils.write(new File(filePath), newText);
+							} catch (IOException e) {
+								JOptionPane.showMessageDialog(null, "Error saving file to Path: " + filePath);
+								e.printStackTrace();
+							}
 						}
 					}
 				}
 			}
 		});
 	}
-	
+
 	public PlainTextDialog(String title, int width, int height, int margin, String text, boolean editable) {
 		this(title, width, height, margin, text, editable, null);
 	}
-	
+
 	public void setText(String text) {
 		textArea.setText(text);
 	}

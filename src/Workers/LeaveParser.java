1ba96daa378b21a08ab4137d748623490e7a1510
Thatchapon Unprasert <poomst@hotmail.com>
2016-07-06 15:05:00 +0700

    LeaveParser for any date.

--- src/Workers/LeaveParser.java
+++ src/Workers/LeaveParser.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Date;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
@@ -11,6 +12,7 @@ import java.util.regex.Pattern;
 import Objects.Status;
 import Objects.Student;
 import Utilities.CommonUtils;
+import Utilities.DateUtils;
 
 public class LeaveParser {
 
@@ -19,7 +21,7 @@ public class LeaveParser {
 	
 	private Map<Integer, Student> leaveStudents = new TreeMap<Integer, Student>();
 
-	public LeaveParser(String dbPath, Map<Integer, Student> students) {
+	public LeaveParser(String dbPath, Map<Integer, Student> students, Date date) {
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(new File(dbPath)));
 			String line;
@@ -33,14 +35,18 @@ public class LeaveParser {
 						System.out.println("I don't think we would face this problem");
 						continue;
 					}
-					students.get(ID).addStatus(new Status(Status.Type.LEAVE, reason));
+					students.get(ID).addStatus(date, new Status(Status.Type.LEAVE, reason));
 					leaveStudents.put(ID, students.get(ID));
 				}
 			}
 			reader.close();
 		} catch (IOException e) {
-			System.out.println("leave.csv for today not found");
+			System.out.println("leave.csv for " + DateUtils.normalFormattedDate(date) + " not found");
 		}
+	}
+	
+	public LeaveParser(String dbPath, Map<Integer, Student> students) {
+		this(dbPath, students, DateUtils.getCurrentDate());
 	}
 
 	public Map<Integer, Student> getLeaveStudents() {

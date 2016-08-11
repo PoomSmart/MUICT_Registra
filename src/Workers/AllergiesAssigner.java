9d9d75b53adf4d48361f19b410caea4d92f17e08
Thatchapon Unprasert <poomst@hotmail.com>
2016-08-11 13:26:27 +0700

    Allergies assignment at runtime.

--- src/Workers/AllergiesAssigner.java
+++ src/Workers/AllergiesAssigner.java
@@ -4,7 +4,24 @@ import java.util.Map;
 
 import Objects.Student;
 
+/**
+ * Runtime modification of freshmen's allergies
+ * 
+ * @author PoomSmart
+ *
+ */
+
 public class AllergiesAssigner {
+	
+	// Serious allergies : stress
+	// ÀÕ∫À◊¥ - they must bring their own cure
+	// Hyperventilation - Fix: Immediately put them into safe and sound environment + use any cover to prevent O2 when breathe
+	// ≈¡∫È“À¡Ÿ - Fix: ,, but don't need cover
+	// Allergic to Aspirin
+	
+	// Ponstan (Fix Menstruation), 1 ID; + Generic type
+	
+	// Reminder: put no stress
 
 	public static Map<Integer, Student> students;
 

591c8ab29783a08ed8dbd888e2bea1edbc6b80aa
Oat <WindowsAndAndroid.app@gmail.com>
2017-08-23 20:31:10 +0700

    set cheer leader complete

--- src/Workers/SpecialAssigner.java
+++ src/Workers/SpecialAssigner.java
@@ -7,9 +7,9 @@ import Objects.Student;
 public class SpecialAssigner {
 
 	public static void assignAll(Map<Integer, Student> db) {
-		/*db.get(Student.createID(29)).setCheerleader(true);
-		db.get(Student.createID(57)).setCheerleader(true);*/
-		//db.get(Student.createID(57)).setCheerleader(true);
+		int IDs[] = { 176, 177, 9, 233,195 };
+		for (int id : IDs)
+			db.get(Student.createID(id)).setCheerleader(true);
 	}
 
 }
